0e696199c7600cf1b8ae8783c3e7965ecd1383ec
ravipatel7636 <78553645+ravipatel7636@users.noreply.github.com>
2022-04-01 19:01:42 -0400

    update dashboard

--- dashboard.py
+++ dashboard.py
@@ -1,49 +1,54 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
-import pandas as pd
-
-
+import contactus
+import About_us
+import home
 base = "dark"
 
 st.set_page_config(
     # page_title="Ex-stream-ly Cool App",
-     page_icon="🧊",
-     layout="wide",
+    page_icon="🧊",
+    layout="wide",
+    initial_sidebar_state='auto'
     # initial_sidebar_state="expanded",
-     #menu_items={
+    # menu_items={
     #     'Get Help': 'https://www.extremelycoolapp.com/help',
-     #    'Report a bug': "https://www.extremelycoolapp.com/bug",
-     #    'About': "# This is a header. This is an *extremely* cool app!"
-     #}
- )
+    #    'Report a bug': "https://www.extremelycoolapp.com/bug",
+    #    'About': "# This is a header. This is an extremely cool app!"
+    # }
+)
 
 
-
-
-#with st.sidebar:
-     #st.title('Home')
-     #st.subheader('Prediction')
-     #st.subheader('Live price')
-     #st.subheader('Comparison')
-     #st.subheader('Crypto Info')
-     #st.subheader('Contact Us')
+# with st.sidebar:
+# st.title('Home')
+# st.subheader('Prediction')
+#st.subheader('Live price')
+# st.subheader('Comparison')
+#st.subheader('Crypto Info')
+#st.subheader('Contact Us')
 selected = option_menu(
-         menu_title=None,
-         options=["Home","Live pricing","Basic Info","Trading tips", "Comparison","Contact us"],
-         default_index=0,
-         orientation="horizontal"
-     )
+
+
+    menu_title=None,
+
+    orientation="horizontal",
+    img = st.image("Images/logo.jpg", use_column_width='Never',width=100),
+    options=["Home", "Live pricing", "Basic Info",
+             "Trading tips", "Comparison", "About Us","Contact us"],
+    default_index=0
+)
+
 if selected == "Home":
-        st.title("home page")
-elif selected =="Live pricing":
-        st.title("This is live pricing section")
+    home.home()
+elif selected == "Live pricing":
+    st.title("This is live pricing section")
 elif selected == "Basic Info":
-        st.title("This is Basic Info page")
+    st.title("This is Basic Info page")
 elif selected == "Trading tips":
-        st.title("This is trading tips pAGE")
+    st.title("This is trading tips pAGE")
+elif selected == "About Us":
+     About_us.aus()
 elif selected == "Comparison":
-        st.title("This is comparison page")
+    st.title("This is comparison page")
 elif selected == "Contact us":
-        st.title("This is contact us page")
-
-
+    contactus.callus()
