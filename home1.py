314e6429d512a2b6378afef0822c2583f45ae137
ravipatel7636 <78553645+ravipatel7636@users.noreply.github.com>
2022-04-02 17:13:47 -0400

    update

--- home1.py
+++ home1.py
@@ -1,7 +1,16 @@
 import streamlit as st
+import streamlit as st
+import pandas as pd
+import numpy as np
+import plotly.express as px
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
+import matplotlib.pyplot as plt
+
+
 st.set_page_config(layout="wide")
 
-col1, col2, col3,col4,col5,col6,col7,col8,col9,col10= st.columns(10)
+col1, col2, col3,= st.columns(3)
 
 
 col1.metric("BTC","$45000" )
@@ -10,12 +19,15 @@ col1.text("This will go in FIRST column")
 col2.metric("ETH", "25000")
 
 col3.metric("DOT","52000" )
+
+col4,col5,col6 =  st.columns(3)
 col4.metric("BTC","$45000")
 col5.metric("BTC","$45000" )
 col6.metric("BTC","$45000" )
+col7,col8,col9 =  st.columns(3)
 col7.metric("BTC","$45000" )
 col8.metric("BTC","$45000" )
 col9.metric("BTC","$45000" )
-col10.metric("BTC","$45000" )
+#col10.metric("BTC","$45000" )
 
 
