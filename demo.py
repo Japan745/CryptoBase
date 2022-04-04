6d794296860ea12c73b38921b9975efee81ee502
Japan1998 <59419608+Japan1998@users.noreply.github.com>
2022-04-04 04:50:42 -0400

    dashboard update

--- demo.py
+++ demo.py
@@ -1,5 +1,3 @@
-import streamlit as st
-from streamlit_option_menu import option_menu
 import contactus
 import About_us
 import basic_info
@@ -7,17 +5,9 @@ import home1
 import trading_tips
 import live_pricing
 import prediction
-
 import streamlit as st
 from streamlit_option_menu import option_menu
-import streamlit.components.v1 as html
 from  PIL import Image
-import numpy as np
-import cv2
-import pandas as pd
-from st_aggrid import AgGrid
-import plotly.express as px
-import io
 im = Image.open("images/ravi.jpg")
 st.set_page_config(
     # page_title="Ex-stream-ly Cool App",
@@ -25,12 +15,15 @@ st.set_page_config(
     layout="wide",
     initial_sidebar_state="auto",)
 
-
-selected = option_menu("Crypto future", ["Home", "Live pricing", "Basic Info", "Prediction", "Trading tips","About Us","Contact us"],
-                         icons=['house', 'activity', 'info', 'book','app-indicator','person lines fill','mailbox'],
-                         menu_icon="currency-bitcoin", default_index=0, orientation = "horizontal",
-                         styles={
-        "container": {"padding": "5!important", "background-color": "light-grey"},
+st.markdown("<center><h1 Style=\"overflow: visible; padding-bottom: 50px; padding-top: 0px;\">Crypto future </h1></center>", unsafe_allow_html=True)
+selected = option_menu(
+        menu_title=None,
+        options=["Home", "Live pricing", "Basic Info", "Prediction", "Trading tips","About Us","Contact us"],
+        icons=['house', 'activity', 'info', 'book','app-indicator','person lines fill','mailbox'],
+        default_index=0,
+        orientation = "horizontal",
+        styles={
+        "container": {"padding": "5!important", "background-color": "white"},
         "icon": {"color": "#2ECC71", "font-size": "25px"},
         "nav-link": {"font-size": "16px", "text-align": "left", "margin":"0px", "--hover-color": "light-grey"},
         "nav-link-selected": {"background-color": "#2ECC71"},
